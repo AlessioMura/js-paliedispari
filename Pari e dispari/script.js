51356c9cfa76868ecdc57b7b16e0f6fd62609e8f
AlessioMura <alessiomura03@gmail.com>
2024-10-04 16:49:31 +0200

    even/odd exercise
    added prep and data phases

---
+++ Pari e dispari/script.js
@@ -0,0 +1,15 @@
+//Pari e Dispari
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+//Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+//Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+
+// Prepazione
+let user_number;
+let user_selection;
+let computer_rnd_number;
+
+
+// Raccolta Dati
+user_selection = confirm('Choose: even(ok) or odd(annulla)?')
+user_number = prompt('Type a number between 1 and 5');
+
