7e59f81020b6e5c0ee22bbb291376a60261f3826
AlessioMura <alessiomura03@gmail.com>
2024-10-04 15:59:44 +0200

    added declaration and invoke phase

--- Palindromo/script.js
+++ Palindromo/script.js
@@ -5,9 +5,17 @@
 let user_word;
 
 // Raccolta Dati
-user_word = prompt('Inserisci quì la parola')
+user_word = prompt('Inserisci quì la parola');
 
 // Elaborazione
+function word_check(user_word) {
+    if (user_word = user_word.split("").reverse().join("")) {
+        document.write('Your word is a palindrome');
+    } else {
+        document.write('Your word is not a palindrome');
+    }
+    return user_word
+}
 
-
-// Output
+// Richiama la funzione
+word_check(user_word);
