e0e109b421ad8a796a7f1db28f0e1d1478ffdaed
AlessioMura <alessiomura03@gmail.com>
2024-10-04 15:28:51 +0200

    initial commit
    added prep and data phases

---
+++ Palindromo/script.js
@@ -0,0 +1,13 @@
+//Palidroma
+//Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+// Preparazione
+let user_word;
+
+// Raccolta Dati
+user_word = prompt('Inserisci quì la parola')
+
+// Elaborazione
+
+
+// Output
